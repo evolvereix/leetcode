318ad1a1d30763fb6a501157a8931ec317b34fd2
zen <0x11cb@gmail.com>
2019-08-19 19:50:00 +0800

    queue

--- algorithms/queue/933-number-of-recent-calls/recentCounter.js
+++ algorithms/queue/933-number-of-recent-calls/recentCounter.js
@@ -29,32 +29,13 @@
  * 
  */
 
-const RecentCounter = function() {
-  this.queue = []
-}
-
 /** 
  * @param {number} t
  * @return {number}
  */
-RecentCounter.prototype.ping = function(t) {
-  this.queue.push(t)
 
-  while (this.queue.length > 0 && this.queue[0] < t - 3000) {
-    this.queue.shift()
-  }
-  return this.queue.length
-}
-
-/** 
- * Your RecentCounter object will be instantiated and called as such:
- * var obj = new RecentCounter()
- * var param_1 = obj.ping(t)
- */
-
-//  use class
 class RecentCounter {
-  constructor(t) {
+  constructor() {
     this.queue = []
   }
 
@@ -66,3 +47,9 @@ class RecentCounter {
     return this.queue.length
   }
 }
+
+/** 
+ * Your RecentCounter object will be instantiated and called as such:
+ * var obj = new RecentCounter()
+ * var param_1 = obj.ping(t)
+ */
