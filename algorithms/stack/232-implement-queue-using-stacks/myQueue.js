ebcbcaebc2917c50bbc0f54ebb83f2063ea19274
zen <0x11cb@gmail.com>
2019-08-20 21:27:32 +0800

    feature queue and other

---
+++ algorithms/stack/232-implement-queue-using-stacks/myQueue.js
@@ -0,0 +1,80 @@
+/**
+ * @source https://leetcode.com/problems/implement-queue-using-stacks/
+ * @author algorizen
+ * @date 2019-08-
+ * @complexity 
+ * 
+ * Implement the following operations of a queue using stacks.
+ * 
+ *  push(x) -- Push element x to the back of queue.
+ *  pop() -- Removes the element from in front of queue.
+ *  peek() -- Get the front element.
+ *  empty() -- Return whether the queue is empty.
+ * 
+ * Note:
+ * 
+ *  You must use only standard operations of a stack -- which means only push to top, peek/pop from top, size, and is empty operations are valid.
+ *  Depending on your language, stack may not be supported natively. You may simulate a stack by using a list or deque (double-ended queue), as long as you use only standard operations of a stack.
+ *  You may assume that all operations are valid (for example, no pop or peek operations will be called on an empty queue).
+ * 
+ * Example:
+ * 
+ *  MyQueue queue = new MyQueue();
+ * 
+ *  queue.push(1);
+ *  queue.push(2);  
+ *  queue.peek();  // returns 1
+ *  queue.pop();   // returns 1
+ *  queue.empty(); // returns false
+ */
+
+/**
+ * Initialize your data structure here.
+ */
+class MyQueue {
+  constructor() {
+
+  }
+
+  /**
+   * Push element x to the back of queue. 
+   * @param {number} x
+   * @return {void}
+   */
+  push(x) {
+      
+  }
+
+  /**
+   * Removes the element from in front of queue and returns that element.
+   * @return {number}
+   */
+  pop() {
+      
+  }
+
+  /**
+   * Get the front element.
+   * @return {number}
+   */
+  peek() {
+      
+  }
+
+  /**
+   * Returns whether the queue is empty.
+   * @return {boolean}
+   */
+  empty() {
+      
+  }
+}
+
+/** 
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
